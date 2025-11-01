29d607d6b3b0212ad1fcb9b86eae0e776344d366
potomnazovy <skeleton39876@mail.ru>
2025-11-01 13:07:05 +0300

    create and destroy matrix

--- main.cpp
+++ main.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
-int main() {
-    
-    return 0;
+#include <cstddef>
+void destroy(int ** mtx, size_t created)
+{
+  for (size_t i = 0; i < created; i++)
+  {
+    delete[] mtx[i];
+  }
+  delete[] mtx;
+}
+int ** create(size_t rows, size_t cols)
+{
+  int ** mtx = new int * [rows];
+  size_t created = 0;
+  try
+  {
+  for (; created < rows; created++)
+  {
+    mtx[created] = new int[cols];
+  }
+}
+  catch (const std::bad_alloc &e)
+  {
+    destroy(mtx, created);
+    throw;
+  }
+  return mtx;
+}
+int main()
+{
+  int ** matrix = nullptr;
+  try
+  {
+    matrix = create(5, 5);
+  }
+  catch (const std::bad_alloc &e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
+  destroy(matrix, 5);
 }
